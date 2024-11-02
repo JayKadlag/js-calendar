f4688a18291c30c28456e84e57bb01d84e366e66
Jay Kadlag <jaykadlag14@gmail.com>
2024-11-02 10:50:45 +0530

    feat: implement render timed events

--- createEventElement.js
+++ createEventElement.js
@@ -1,3 +1,5 @@
+import {format, parse} from 'date-fns';
+
 export default function createEventElement(event) {
 	return event.isAllDay
 		? createAllDayEventElement(event)
@@ -5,6 +7,7 @@ export default function createEventElement(event) {
 }
 
 const allDayEventTemplate = document.getElementById('all-day-event-template');
+
 function createAllDayEventElement(event) {
 	const element = allDayEventTemplate.content
 		.cloneNode(true)
@@ -15,4 +18,17 @@ function createAllDayEventElement(event) {
 	return element;
 }
 
-function createTimedEventElement(event) {}
+const timedEventTemplate = document.getElementById('timed-event-template');
+function createTimedEventElement(event) {
+	const element = timedEventTemplate.content
+		.cloneNode(true)
+		.querySelector('[data-event]');
+	element.querySelector('[data-name]').textContent = event.name;
+	element.querySelector('[data-color]').classList.add(event.color);
+	element.querySelector('[data-time]').textContent = format(
+		parse(event.startTime, 'HH:mm', event.date),
+		'h:mmaaa'
+	);
+
+	return element;
+}
